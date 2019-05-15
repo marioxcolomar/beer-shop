274ec0a9095114deb06c0eb29bc5595421938cf8
Mario Coloma <marioxcolomar@gmail.com>
2019-05-15 12:06:27 +0200

    Add shop to container and set beers state.
    Dispatch beer and shop actions to props.

---
+++ src/__containers/shop.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as beerActions from '../__actions/beerActions'
+import * as shopActions from '../__actions/shopActions'
+
+import ShopComponent from '../components/Shop'
+
+const mapStateToProps = state => {
+  return {
+    beers: state.beers,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    beerActions: bindActionCreators(beerActions, dispatch),
+    shopActions: bindActionCreators(shopActions, dispatch),
+  }
+}
+
+const Shop = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShopComponent)
+
+export default Shop
