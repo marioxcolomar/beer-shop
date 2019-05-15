dbfcfd7ca294d710f301bf16c265722bdf899c2e
Mario Coloma <marioxcolomar@gmail.com>
2019-05-15 14:51:36 +0200

    Fix naming of fetch actions to plural.

--- src/__actions/beerActions.js
+++ src/__actions/beerActions.js
@@ -1,32 +1,32 @@
-export const FETCH_BEER_BEGIN = 'FETCH_BEER_BEGIN'
-export const FETCH_BEER_SUCCESS = 'FETCH_BEER_SUCCESS'
-export const FETCH_BEER_FAILURE = 'FETCH_BEER_FAILURE'
+export const FETCH_BEERS_BEGIN = 'FETCH_BEERS_BEGIN'
+export const FETCH_BEERS_SUCCESS = 'FETCH_BEERS_SUCCESS'
+export const FETCH_BEERS_FAILURE = 'FETCH_BEERS_FAILURE'
 
-export const fetchBeerBegin = () => ({
-  type: FETCH_BEER_BEGIN
+export const fetchBeersBegin = () => ({
+  type: FETCH_BEERS_BEGIN
 })
 
-export const fetchBeerSuccess = beers => ({
-  type: FETCH_BEER_SUCCESS,
+export const fetchBeersSuccess = beers => ({
+  type: FETCH_BEERS_SUCCESS,
   payload: { beers }
 })
 
-export const fetchBeerFailure = error => ({
-  type: FETCH_BEER_FAILURE,
+export const fetchBeersFailure = error => ({
+  type: FETCH_BEERS_FAILURE,
   payload: { error }
 })
 
 export function fetchBeers() {
   return dispatch => {
-    dispatch(fetchBeerBegin())
+    dispatch(fetchBeersBegin())
     return fetch(process.env.REACT_APP_PUNK_BEER_URL)
       .then(handleErrors)
       .then(res => res.json())
       .then(json => {
-        dispatch(fetchBeerSuccess(json.beers))
+        dispatch(fetchBeersSuccess(json.beers))
           return json.beers
       })
-      .catch(error => dispatch(fetchBeerFailure(error)))
+      .catch(error => dispatch(fetchBeersFailure(error)))
   }
 }
 
