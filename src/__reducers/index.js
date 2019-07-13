b4248f779d854169d6ad5a4f92d6a9d14069cfd6
mariocoloma <mario@hairpal.dk>
2019-07-13 09:30:21 +0200

    Fix naming of reducer. (no idea why it is like this)

--- src/__reducers/index.js
+++ src/__reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-import beerReducer from './beerReducer'
+import beers from './beerReducer'
 
 export default combineReducers({
-  beerReducer,
+  beers,
 })
