de9bfa369ffd9b81563f0f622aee3d7dfaf5bd81
marioxcolomar <marioxcolomar@gmail.com>
2019-11-16 00:35:37 +0100

    Add buttons to item cards and price temporary placeholder.

--- src/components/ShopItem/index.js
+++ src/components/ShopItem/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { FaCartPlus } from 'react-icons/fa'
+import { MdFavorite } from 'react-icons/md'
 
 import './styles.scss'
 
 const ShopItem = ({ imgSrc, name, description }) => {
-  return (
-    <div id='item-card'>
-      <img alt={imgSrc} src={imgSrc} />
-      <h3>{name}</h3>
-    </div>
-  )
+	return (
+		<div id='item-card'>
+			<img alt={imgSrc} src={imgSrc} />
+			<h3>{name}</h3>
+			<p><i>price</i></p>
+			<button type="button" ><FaCartPlus /></button>
+			<button type="button" style={{ backgroundColor: '#CD5C5C' }} ><MdFavorite /></button>
+		</div>
+	)
 }
 
 export default ShopItem
