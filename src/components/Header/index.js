2aafe955b4ee9b31e32ffcd3d046bcd1af0bfab9
marioxcolomar <marioxcolomar@gmail.com>
2019-11-15 23:19:32 +0100

    Added styles to header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import './styles.scss'
+
 export default function Header() {
-  return (
-    <div className='header'>
-      <p>Header</p>
-    </div>
-  )
+	return (
+		<div className='header'>
+			<p>Header</p>
+		</div>
+	)
 }
