1af7d1e1b03719867a8f3d7bc1ee2e2a3d08bbcd
marioxcolomar <marioxcolomar@gmail.com>
2019-12-08 23:42:46 +0100

    Decrease name of beer to 15 characters.

--- src/components/ShopItem/index.js
+++ src/components/ShopItem/index.js
@@ -8,7 +8,7 @@ const ShopItem = ({ imgSrc, name, description }) => {
 	return (
 		<div id='item-card'>
 			<img alt={imgSrc} src={imgSrc} />
-			<h3>{name}</h3>
+			<h3>{name.length > 15 ? name.substring(0, 15).concat('...') : name}</h3>
 			<p><i>price</i></p>
 			<button type="button" ><FaCartPlus /></button>
 			<button type="button" style={{ backgroundColor: '#CD5C5C' }} ><MdFavorite /></button>
