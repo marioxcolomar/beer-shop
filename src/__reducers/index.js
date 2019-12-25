67c0e24558772f5d9dd1984fae12fd268f8caa5a
marioxcolomar <marioxcolomar@gmail.com>
2019-12-25 19:02:39 +0100

    Add cart and wishlist reducers to root reducer.

--- src/__reducers/index.js
+++ src/__reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux'
 
 import beers from './beerReducer'
+import cart from './cartReducer'
+import wishlist from './wishListReducer'
 
 export default combineReducers({
   beers,
+  cart,
+  wishlist
 })
