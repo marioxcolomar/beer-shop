7f80077fc9827f9373aef6027084b812ed257bcc
marioxcolomar <marioxcolomar@gmail.com>
2019-12-25 19:01:19 +0100

    Fix indentation.

--- src/__reducers/beerReducer.js
+++ src/__reducers/beerReducer.js
@@ -1,41 +1,41 @@
 import {
-  FETCH_BEERS_BEGIN,
-  FETCH_BEERS_SUCCESS,
-  FETCH_BEERS_FAILURE
-} from '../__actions/beerActions'
+	FETCH_BEERS_BEGIN,
+	FETCH_BEERS_SUCCESS,
+	FETCH_BEERS_FAILURE
+} from '../__actions/beerActions';
 
 const initialState = {
-  beers: [],
-  loading: false,
-  error: null,
-}
+	beers: [],
+	loading: false,
+	error: null
+};
 
 export default function beerReducer(state = initialState, action) {
-  switch(action.type) {
-    case FETCH_BEERS_BEGIN:
-      return {
-        ...state,
-        loading: true,
-        error: null
-      };
+	switch (action.type) {
+		case FETCH_BEERS_BEGIN:
+			return {
+				...state,
+				loading: true,
+				error: null
+			};
 
-    case FETCH_BEERS_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        beers: action.payload,
-        error: null
-      };
+		case FETCH_BEERS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				beers: action.payload,
+				error: null
+			};
 
-    case FETCH_BEERS_FAILURE:
-      return {
-        ...state,
-        loading: false,
-        error: action.payload.error,
-        beers: []
-      };
+		case FETCH_BEERS_FAILURE:
+			return {
+				...state,
+				loading: false,
+				error: action.payload.error,
+				beers: []
+			};
 
-      default:
-        return state;
-  }
+		default:
+			return state;
+	}
 }
